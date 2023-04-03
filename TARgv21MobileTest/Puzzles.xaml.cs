80c51cd01f18d927614433283bd2b8eb03429366
daaniel124 <daaniel124@gmail.com>
2023-04-03 14:21:08 +0300

    Puzzles project

--- TARgv21MobileTest/Puzzles.xaml.cs
+++ TARgv21MobileTest/Puzzles.xaml.cs
@@ -12,14 +12,18 @@ namespace TARgv21MobileTest
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Puzzles : ContentPage
     {
+        int answersTrue;
+        int answers;
+
         [Obsolete]
         public Puzzles()
         {
             Label lbl = new Label
             {
-                Text = "Загадки",
+                Text = "Примеры",
                 VerticalOptions = LayoutOptions.Center,
-                HorizontalOptions = LayoutOptions.Center
+                HorizontalOptions = LayoutOptions.Center,
+                FontSize = Device.GetNamedSize(NamedSize.Title, typeof(Label))
             };
 
             Button alertButton = new Button
@@ -30,35 +34,49 @@ namespace TARgv21MobileTest
             };
             alertButton.Clicked += AlertButton_Clicked;
 
-            Button alertQuestionButton = new Button
-            {
-                Text = "Вопросы",
-                VerticalOptions = LayoutOptions.Start,
-                HorizontalOptions = LayoutOptions.Center
-            };
-            alertQuestionButton.Clicked += AlertQuestionButton_Clicked;
-            Content = new StackLayout { Children = { lbl, alertQuestionButton } };
+            Content = new StackLayout { Children = { lbl, alertButton } };
         }
 
         [Obsolete]
         private async void AlertButton_Clicked(object sender, EventArgs e)
         {
+            answers = 0;
+            answersTrue = 0;
             bool result = await DisplayAlert("Примеры", "Вам будет предложено решить 10 примеров. Хотите начать?", "Да", "Нет");
             if (result == true)
             {
                 Random rd = new Random();
-                for (int i = 0; i < 10; i++)
+                int i;
+                for (i = 1; i < 11;)
                 {
                     int rand_num_1 = rd.Next(0, 10);
                     int rand_num_2 = rd.Next(0, 10);
-                    string userResult = await DisplayPromptAsync("Вопрос #" + i, rand_num_1 + " * " + rand_num_2 + " = ?", "Ответить", "Пропустить", "Ответ", maxLength: 3, keyboard: Keyboard.Numeric);
+                    string userResult = await DisplayPromptAsync("Вопрос #" + (i), rand_num_1 + " * " + rand_num_2 + " = ?", "Ответить", "Выйти", "Ответ", maxLength: 3, keyboard: Keyboard.Numeric);
                     int randResult = rand_num_1 * rand_num_2;
                     int userResultInt = int.Parse(userResult);
+                    bool resultAnswer;
                     if (randResult == userResultInt)
                     {
-
+                        resultAnswer = await DisplayAlert("Вопрос #" + (i), "Ответ правильный", "Продолжить", "Выйти");
+                        answersTrue++;
+                        answers++;
                     }
+                    else
+                    {
+                        resultAnswer = await DisplayAlert("Вопрос #" + (i), "Ответ не правильный", "Продолжить", "Выйти");
+                        answers++;
+                    }
+                    if (resultAnswer == true)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        i = 11;
+                    }
+                   
                 }
+                _ = DisplayAlert("Примеры", "Вы ответили правильно на " + answersTrue + " пример/ов из " + answers, "Ок");
             }
         }
 
